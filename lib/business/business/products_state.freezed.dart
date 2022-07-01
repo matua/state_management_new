cfcdd6dd9e1174979652339aba55f1a9c1fc6b7b
Georgii <matuageorge@gmail.com>
2022-07-01 13:00:39 +0200

    minor refactoring

---
+++ lib/business/business/products_state.freezed.dart
@@ -0,0 +1,141 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'products_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$ProductsState {
+  List<Product> get products => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ProductsStateCopyWith<ProductsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProductsStateCopyWith<$Res> {
+  factory $ProductsStateCopyWith(
+          ProductsState value, $Res Function(ProductsState) then) =
+      _$ProductsStateCopyWithImpl<$Res>;
+
+  $Res call({List<Product> products});
+}
+
+/// @nodoc
+class _$ProductsStateCopyWithImpl<$Res>
+    implements $ProductsStateCopyWith<$Res> {
+  _$ProductsStateCopyWithImpl(this._value, this._then);
+
+  final ProductsState _value;
+
+  // ignore: unused_field
+  final $Res Function(ProductsState) _then;
+
+  @override
+  $Res call({
+    Object? products = freezed,
+  }) {
+    return _then(_value.copyWith(
+      products: products == freezed
+          ? _value.products
+          : products // ignore: cast_nullable_to_non_nullable
+              as List<Product>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_ProductsStateCopyWith<$Res>
+    implements $ProductsStateCopyWith<$Res> {
+  factory _$$_ProductsStateCopyWith(
+          _$_ProductsState value, $Res Function(_$_ProductsState) then) =
+      __$$_ProductsStateCopyWithImpl<$Res>;
+
+  @override
+  $Res call({List<Product> products});
+}
+
+/// @nodoc
+class __$$_ProductsStateCopyWithImpl<$Res>
+    extends _$ProductsStateCopyWithImpl<$Res>
+    implements _$$_ProductsStateCopyWith<$Res> {
+  __$$_ProductsStateCopyWithImpl(
+      _$_ProductsState _value, $Res Function(_$_ProductsState) _then)
+      : super(_value, (v) => _then(v as _$_ProductsState));
+
+  @override
+  _$_ProductsState get _value => super._value as _$_ProductsState;
+
+  @override
+  $Res call({
+    Object? products = freezed,
+  }) {
+    return _then(_$_ProductsState(
+      products: products == freezed
+          ? _value._products
+          : products // ignore: cast_nullable_to_non_nullable
+              as List<Product>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ProductsState implements _ProductsState {
+  _$_ProductsState({required final List<Product> products})
+      : _products = products;
+
+  final List<Product> _products;
+
+  @override
+  List<Product> get products {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_products);
+  }
+
+  @override
+  String toString() {
+    return 'ProductsState(products: $products)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ProductsState &&
+            const DeepCollectionEquality().equals(other._products, _products));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(_products));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_ProductsStateCopyWith<_$_ProductsState> get copyWith =>
+      __$$_ProductsStateCopyWithImpl<_$_ProductsState>(this, _$identity);
+}
+
+abstract class _ProductsState implements ProductsState {
+  factory _ProductsState({required final List<Product> products}) =
+      _$_ProductsState;
+
+  @override
+  List<Product> get products => throw _privateConstructorUsedError;
+
+  @override
+  @JsonKey(ignore: true)
+  _$$_ProductsStateCopyWith<_$_ProductsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
